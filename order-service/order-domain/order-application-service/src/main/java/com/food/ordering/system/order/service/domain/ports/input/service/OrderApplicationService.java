346028d5c8b8eaf5422563a73dc843618368d04c
Santiago <davids.guerrero@udea.edu.co>
2022-10-13 10:07:51 -0500

    Commit class

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
@@ -11,4 +11,6 @@ public interface OrderApplicationService {
     CreateOrderResponse createOrder(@Valid CreateOrderCommand createOrderCommand);
 
     TrackOrderResponse trackOrder(@Valid TrackOrderQuery trackOrderQuery);
+
+
 }
